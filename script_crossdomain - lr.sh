0a222d249c995382c49d6de94414901a0aede446
IgorSouza21 <igor11souza@gmail.com>
2019-02-28 00:07:08 -0300

    comented Neural Network

--- script_crossdomain - lr.sh
+++ script_crossdomain - lr.sh
@@ -34,8 +34,9 @@ conda install -c anaconda six=1.11.0 && \
 conda install -c anaconda numpy=1.14.2 && \
 conda install -c anaconda pickle &&\
 ###### Novas bibliotecas adicionadas ######
-conda install -c keras=2.2.4 &&\
-conda install -c pandas=0.23.4 &&\
+conda install tensorflow=1.12.0 &&\
+conda install keras=2.2.4 &&\
+conda install pandas=0.23.4 &&\
 conda install -c anaconda nltk=3.3 &&\
 python -m nltk.downloader all &&\
 conda install -c anaconda scipy=1.2.0 &&\
